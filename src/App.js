581206da7ec59261866ee5979886d6ccaf624393
CSK <csk@users.noreply.github.com>
2020-05-24 08:46:29 -0500

    Update App.js
    1. Function component to Class Component
    2. initial state
    3. onClick event to change state with setState()

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+// function component
+/* function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Hello, CSK!!!</p>
     </div>
   );
+} */
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Chiru"
+    }
+  }
+
+  changeName() {
+    this.setState({name: 'CSK'});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>Hello, {this.state.name}!!!</p>
+        <button onClick={() => this.changeName()}>Change Name</button>
+      </div>
+    )
+  }
 }
 
 export default App;
