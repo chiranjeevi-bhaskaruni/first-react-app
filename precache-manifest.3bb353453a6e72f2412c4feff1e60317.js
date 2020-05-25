a584c8b64d56267ba36daa34e918878017bc0fec
chiranjeevi-bhaskaruni <bhaskarunichiranjeevi@gmail.com>
2020-05-24 20:12:36 -0500

    Updates

---
+++ precache-manifest.3bb353453a6e72f2412c4feff1e60317.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a862b671b961f524a8a9743b24e28ce",
+    "url": "/chiranjeevi-bhaskaruni/first-react-app/index.html"
+  },
+  {
+    "revision": "0eeaad578161e0f69636",
+    "url": "/chiranjeevi-bhaskaruni/first-react-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1db876697506fd8cd8a4",
+    "url": "/chiranjeevi-bhaskaruni/first-react-app/static/css/main.14252c81.chunk.css"
+  },
+  {
+    "revision": "0eeaad578161e0f69636",
+    "url": "/chiranjeevi-bhaskaruni/first-react-app/static/js/2.878b5121.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/chiranjeevi-bhaskaruni/first-react-app/static/js/2.878b5121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db876697506fd8cd8a4",
+    "url": "/chiranjeevi-bhaskaruni/first-react-app/static/js/main.dbdd3243.chunk.js"
+  },
+  {
+    "revision": "55df3e2e877ac4808b2f",
+    "url": "/chiranjeevi-bhaskaruni/first-react-app/static/js/runtime-main.58044cb4.js"
+  }
+]);
